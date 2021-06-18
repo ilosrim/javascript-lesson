072c5973ce853fd6d51a1fb6a7209f97ea112ff5
ilosrim <mirsolikhdev@gmail.com>
2021-06-18 19:56:53 +0500

    Vazifa

---
+++ lesson-2/index.js
@@ -0,0 +1,53 @@
+// 14
+// L=2*PI*R, S=PI*R*2
+// let L = 15
+// let R = L/(2*Math.PI)
+// let S = Math.PI*(Math.pow(R,2))
+// console.log('Radius: '+R, 'Yuza: '+S)
+
+// 15
+// let S = 17
+// let R = S/Math.PI
+// console.log('Radius: '+Math.sqrt(R))
+
+// 16
+// |X2-X1|
+// let x1 = 10
+// let x2 = -10
+// console.log(`Masofa: ${x1-x2}`)
+
+// 17
+// let a = 2
+// let b = 6
+// let c = 10
+
+// let ac = c-a
+// let bc = c-b
+// let count = ac + bc
+// console.log(`AC kesma uzunligi: ${ac}, BC kesma uzunligi: ${bc}, Kesmalar uzunligi yig'indisi: ${count}`)
+
+// 18
+// let a = 2
+// let c = 4
+// let b = 6
+
+// let ac = c-a
+// let bc = b-c
+// console.log(`AC va BC kesmalar ko'paytmasi: ${ac*bc}`)
+
+// 19
+// S = a*b, P = 2*(a+b)
+// let a = 15
+// let b = 21
+// let S = a*b
+// let P = 2*(a+b)
+// console.log(`To'g'ri to'rtburchak perimetri: ${P}, yuzasi: ${S}`)
+
+// 20
+// let x1 = 1
+// let x2 = 6
+// let y1 = -8
+// let y2 = 7
+// console.log(Math.round(Math.sqrt((Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2)))))
+
+// 21
