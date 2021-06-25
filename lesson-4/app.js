c12d9632c5eee891742ff6c94cd934144892ad59
ilosrim <mirsolikhdev@gmail.com>
2021-06-25 15:11:42 +0500

    news

--- lesson-4/app.js
+++ lesson-4/app.js
@@ -38,7 +38,42 @@ for (let i=0; i<100; i++){
     }
 }
 
-let str = `asror Doston Xoliq Nematullo baxrom`
+let str = `asror Doston Xoliq Nematullo baxrom joxon azim Turg'un eshmat Shuhrat`
 let newArr = str.split(` `)
 newArr.sort( (a, b) => a.localeCompare(b, 'en', {ignorePunctuation: true}));
 console.log(newArr)
+
+// Masalalar
+
+// 1
+// 1m = 100cm
+let L = 1000
+let m = (L*1)/100
+console.log(`${m} metr`)
+
+// 2
+// 1t = 1000kg
+let M = 10
+let T = (M*1)/1000
+console.log(`${T} tonna`)
+
+// 4
+let A = 10
+let B = 2
+console.log(`A kesmada B kesma ${A/B} marta joylashishi mumkin`)
+
+// 5
+console.log(`A kesmada B kesma ${A/B} marta joylashishi mumkin. A kesmada B kesmaning ${(A/B)-B} qisimlari joylashmagan`)
+
+// 6
+// ikki xonali son berilgan, avval o'nliklar xonsidagi raqami, keyin birliklar xonasidagi raqami chiqarilsin
+let num = 21
+console.log(`O'nliklar xonasidagi raqam: ${num.toString()[0]}, birliklar xonasidagi raqam: ${num.toString()[1]}`)
+
+// 7
+let numStr = num.toString().split('')
+let a = numStr[0], b = numStr[1]
+
+let num1 = parseInt(a, 10)
+// console.log(Number.parseInt(a))
+console.log(typeof a)
