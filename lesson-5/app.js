3ee5ba8c10953cc5fe1fc3b37fd7607044635fb3
ilosrim <mirsolikhdev@gmail.com>
2021-07-01 11:48:29 +0500

    first commit

---
+++ lesson-5/app.js
@@ -0,0 +1,13 @@
+let a = 'BCC'
+let b = 'BAC'
+let a1 = a.split('').sort()
+let a2 = b.split('').sort()
+function test(){
+    for(let i=0; i<a1.length; i++){
+        for(let j=0; j<a2.length; j++){
+            if(a1[i]===a2[i]) return true;
+            else return false;
+        }
+    }
+}
+console.log(test())
