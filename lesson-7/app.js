bf9027cf3e6ffa8ea2decdee9f31846c115f8101
ilosrim <mirsolikhdev@gmail.com>
2021-07-02 20:57:30 +0500

    days 07

--- lesson-7/app.js
+++ lesson-7/app.js
@@ -7,29 +7,22 @@ str1.length===str2.length && str1.split('').sort().join()===str2.split('').sort(
 // 2
 const array = [22,4,65,3,66,5,95,959,2,7,11,13];
 
-function isPrime(num) {
-    for (let i = 2; num > i; i++) {
-        if (num % i == 0) {
-            return false;
-        }
-    }
+// 2.1
+const isPrime = num => {
+    for (let i = 2; num > i; i++) num % i == 0 && false;
     return num > 1
-}
-console.log(array.filter(isPrime))
+};
+console.log(`Tub sonlar: ${array.filter(isPrime).sort((a,b)=>a-b)}`)
 
-// 3
-function toq(){
-    return array.filter((value)=>value % 2 !== 0).sort((a,b)=>a-b)
-}
-console.log(toq())
+// 2.2
+const toq = () => array.filter((value)=>value % 2 !== 0).sort((a,b)=>a-b)
+console.log(`Toq sonalr: ${toq()}`)
 
-// 4
-function juft(){
-    return array.filter((value)=>value % 2 === 0).sort((a,b)=>a-b)
-}
-console.log(juft())
+// 2.3
+const juft = () => array.filter((value)=>value % 2 === 0).sort((a,b)=>a-b)
+console.log(`Juft sonalr: ${juft()}`)
 
+// 
 const mevalar = ['olma', 'anor', 'anjiraaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa']
 let str = mevalar.join()
- 
-console.log(str.match(/['a']/gi).length);
+console.log(`A harfi soni: ${str.match(/['a']/gi).length}`);
