6a8ab87d98f93f336f326eb4762830f15f9dc3e5
ilosrim <mirsolikhdev@gmail.com>
2021-07-07 18:31:28 +0500

    Vazifa 8

--- lesson-8/app.js
+++ lesson-8/app.js
@@ -1,11 +1,25 @@
 let arr = ['apple', 'orange', 'Apelsin', 'ananas', 'bodom']
 
-let str = arr.join()
-// console.log(`A harfi soni: ${str.match(/['a']/gi).length}`);
+// let str = arr.join()
+// // console.log(`A harfi soni: ${str.match(/['a']/gi).length}`);
 
-const find = (arr) => {
-    let strA = arr.join().match(/['a']/gi).length
-    let strB = arr.join().match(/['b']/gi).length
-    console.log(`A harflar soni:`,strA, `/ B harflar soni:`, strB);
-}
-find(arr)
+// const find = (arr) => {
+//     let strA = arr.join().match(/['a']/gi).length
+//     let strB = arr.join().match(/['b']/gi).length
+//     console.log(`A harflar soni:`,strA, `/ B harflar soni:`, strB);
+// }
+// find(arr)
+
+let findLetter = 'a'
+arr.forEach((value, index)=>{
+    let count = 0
+    for(let i=0; i<value.length; i++){
+        if(value[i].toLowerCase()===findLetter.toLowerCase()){
+            count++
+        }else{
+            continue;
+        }
+    }
+    console.log(index, count);
+})
+
