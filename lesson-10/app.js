13f7f9e458eb0ef3a686551fa0fd10e765d90a26
ilosrim <mirsolikhdev@gmail.com>
2021-07-18 12:08:56 -0400

    vazifa 10

--- lesson-10/app.js
+++ lesson-10/app.js
@@ -177,7 +177,41 @@ for(let i = 0; i<8; i++){
 }
 console.log(result);
 
-// vazifa
+// vazifa 1
 let gap = `O'zbekistonning 2021-yildagi jami qarzi $25 mlrdga yetdi`
 let son = gap.match(/[0-9]/g).map(val=>Number(val))
 console.log(son);
+
+// vazifa 2
+// Ixtiyoriy so'zni ichida nechta unli harf borligi aniqlansin.
+// Bunda so'z o'zbek lotin alifbosida ekanligi hisobga olinsin.
+
+let arr = [`a`,`e`,`i`,`u`,`o'`,`o`];
+const tekshir = gapArr.map(val=>console.log(val))
+console.log(gapArr.forEach(val=>console.log(val)));
+
+let gap = 'Assalomu alaykum';
+// 1-usul
+let unlilarSoni = gap.match(/[`a`|`e`|`i`|`u`|`o`|`o'`|`u`]/gi).length
+console.log(unlilarSoni);
+
+// 2-usul
+let gapArr = gap.split('');
+const uSoni = gapArr.reduce((count, val) => {
+    if(val.toLocaleLowerCase()=='a' || val.toLocaleLowerCase()=='e' || val.toLocaleLowerCase()=='i' || val.toLocaleLowerCase()==`o'` || val.toLocaleLowerCase()=='o' || val.toLocaleLowerCase()=='u'){
+        count+=val.length
+    }
+    return count
+},0)
+console.log(uSoni);
+
+
+let gap = 'Assalomu alaykum';
+let gapArr = gap.split('');
+let count = 0;
+for(const i of gapArr){
+    if(i.toLocaleLowerCase()=='a' || i.toLocaleLowerCase()=='e' || i.toLocaleLowerCase()=='i' || i.toLocaleLowerCase()==`o'` || i.toLocaleLowerCase()=='o' || i.toLocaleLowerCase()=='u'){
+        count+=i.length
+    }
+}
+console.log(count);
