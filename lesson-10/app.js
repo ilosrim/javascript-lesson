99d03fbccee8cfda588109523b5de565d31326d7
ilosrim <mirsolikhdev@gmail.com>
2021-07-20 23:05:16 -0400

    Prime numbers

--- lesson-10/app.js
+++ lesson-10/app.js
@@ -218,15 +218,13 @@
 
 // Tub son topish funksiyasi
 function showPrimeNumbers(limit){
-    for (let number = 2; number <= limit; number++){
-        let isPrime = true;
-        for (let factor = 2; factor < number; factor++){
-            if(number%factor===0){
-                isPrime = false;
-                break;
-            }
-        }
-        if (isPrime) console.log(number);
-    }
+    for (let number = 2; number <= limit; number++)
+        if (isPrime(number)) console.log(number);
+}
+
+function isPrime(number){
+    for (let factor = 2; factor < number; factor++)
+        if(number%factor===0)   return false;
+        return true;
 }
 showPrimeNumbers(20);
